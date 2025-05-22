13e3222972c5bcd79e786ad437b18988bdc91c02
Hari Reddy <harireddyk1@gmail.com>
2025-05-22 14:22:38 -0400

    folder structure added

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='GenerativeAI',
+    version= '0.0.0',
+    author= 'Hari',
+    author_email= 'harireddyk1@gmail.com',
+    packages= find_packages(),
+    install_requires= []
+)
